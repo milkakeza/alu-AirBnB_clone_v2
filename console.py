ffc82703e76124026a0873ddc0f73a6fdcf678fe
DLOADIN <m.david@alustudent.com>
2025-01-21 13:20:24 +0200

    new changes

--- console.py
+++ console.py
@@ -113,40 +113,40 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
-    def do_create(self, args):
-        """ Create an object of any class"""
-        args = args.split()
-        if not args:
-            print("** class name missing **")
-            return
-        elif args[0] not in HBNBCommand.classes:
-            print("** class doesn't exist **")
-            return
+    def do_create(self, line):
+        """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
+        Create a new class instance with given keys/values and print its id.
+        """
+        try:
+            if not line:
+                raise SyntaxError()
+            my_list = line.split(" ")
 
-        kwargs = {}
-        for arg in args[1:]:
-            if '=' in arg:
-                key, value = arg.split('=', 1)
-                key = key.replace('_', ' ')
+            kwargs = {}
+            for i in range(1, len(my_list)):
+                key, value = tuple(my_list[i].split("="))
                 if value[0] == '"':
-                    value = value[1:-1].replace('\\"', '"')
-                elif '.' in value:
-                    try:
-                        value = float(value)
-                    except ValueError:
-                        continue
+                    value = value.strip('"').replace("_", " ")
                 else:
                     try:
-                        value = int(value)
-                    except ValueError:
+                        value = eval(value)
+                    except (SyntaxError, NameError):
                         continue
                 kwargs[key] = value
 
-        new_instance = HBNBCommand.classes[args[0]](**kwargs)
-        storage.save()
-        print(new_instance.id)
-        storage.save()
+            if kwargs == {}:
+                obj = eval(my_list[0])()
+            else:
+                obj = eval(my_list[0])(**kwargs)
+                storage.new(obj)
+            print(obj.id)
+            obj.save()
 
+        except SyntaxError:
+            print("** class name missing **")
+        except NameError:
+            print("** class doesn't exist **")
+            
     def help_create(self):
         """ Help information for the create method """
         print("Creates a class of any type")
