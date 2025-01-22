cc08aa09c30ab9a927a06faf6242cb32e5a3fd0d
milkakeza <m.isingizwe1@alustudent.com>
2025-01-22 16:22:33 +0200

    FileStorageUpdated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,6 +18,7 @@ class FileStorage:
                 if cls == value.__class__ or cls == value.__class__.__name__:
                     new_dict[key] = value
             return new_dict
+    
     def new(self, obj):
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
@@ -51,7 +52,7 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
