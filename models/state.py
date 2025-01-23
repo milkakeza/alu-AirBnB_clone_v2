97239c8f24927af51014ff13630673b7e28778f0
DLOADIN <m.david@alustudent.com>
2025-01-24 01:11:52 +0200

    new onw

--- models/state.py
+++ models/state.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-
+import models
 from models.base_model import BaseModel, Base
+import os
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
-import models
-import os
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
+
+    __tablename__ = "states"
 
     name = Column(String(128), nullable=False)
 
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship('City', backref='state',
-                              cascade='all, delete-orphan')
-
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref="state",
+                              cascade="all, delete, delete-orphan")
     else:
         @property
         def cities(self):
