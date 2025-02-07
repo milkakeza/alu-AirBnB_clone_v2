993b5691a288b4a0eec43bc832f3c31aa0ee72ef
milkakeza <m.isingizwe1@alustudent.com>
2025-02-07 21:15:52 +0000

    SetUp

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,15 +10,16 @@ sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/releases/test/index.html
-echo "<html>
-<head></head>
-<body>
-  <p>Holberton School</p>
-</body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head></head>
+  <body>
+    <p>Holberton School</p>
+  </body>
+ </html>" | sudo tee /data/web_static/releases/test/index.html
 
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/listen 80 default-server/a location/hbnb_static {alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
