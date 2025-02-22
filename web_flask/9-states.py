e788f27c18d3f63196bed5a12d5df1bdb66fc0ac
milkakeza <m.isingizwe1@alustudent.com>
2025-02-22 23:19:40 +0200

    States and State

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -12,7 +12,9 @@ app = Flask(__name__)
 
 @app.route('/states', strict_slashes=False)
 def states():
-    """Comment"""
+    
+    """Renders a template displaying a list of all states."""
+
     return render_template('9-states.html',
                            states=storage.all('State').values())
 
@@ -25,7 +27,17 @@ def teardown(self):
 
 @app.route('/states/<id>', strict_slashes=False)
 def states_id(id):
-    """Comment"""
+    
+    """Renders a template displaying the state with the given ID and its cities.
+
+    If a state with the specified ID exists, the template is rendered with the state's
+    details and associated cities. If no such state is found, it renders a template
+    indicating that the state was not found.
+    
+    Args:
+        id (str): The ID of the state to be displayed.
+    """
+
     for state in storage.all(State).values():
 	if state.id == id:
 	    return render_template('9-states.html', state=state)
@@ -33,4 +45,4 @@ def states_id(id):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
